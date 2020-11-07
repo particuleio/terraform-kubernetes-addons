9322cfa633c4231447a019fe0571575322946d47
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-11-07 17:49:07 +0100

    feat: add generic kube-prometheus-stack
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- kube-prometheus.tf
+++ kube-prometheus.tf
@@ -1,27 +1,20 @@
 locals {
-  prometheus_operator = merge(
+  kube-prometheus-stack = merge(
     local.helm_defaults,
     {
-      name                   = "prometheus-operator"
+      name                   = "kube-prometheus-stack"
       namespace              = "monitoring"
-      chart                  = "prometheus-operator"
+      chart                  = "kube-prometheus-stack"
       repository             = "https://prometheus-community.github.io/helm-charts"
-      kiam_allowed_regexp    = "^$"
       enabled                = false
-      chart_version          = "v9.3.1"
-      allowed_cidr           = "0.0.0.0/0"
+      chart_version          = "11.0.2"
+      allowed_cidrs          = ["0.0.0.0/0"]
       default_network_policy = true
     },
-    var.prometheus_operator
+    var.kube-prometheus-stack
   )
 
-  values_prometheus_operator = <<VALUES
-kubeScheduler:
-  enabled: false
-kubeControllerManager:
-  enabled: false
-kubeEtcd:
-  enabled: false
+  values_kube-prometheus-stack = <<VALUES
 grafana:
   rbac:
     pspUseAppArmor: false
@@ -30,8 +23,8 @@ grafana:
     dashboardproviders.yaml:
       apiVersion: 1
       providers:
-      - name: 'default' # Configure a dashboard provider file to
-        orgId: 1        # put Kong dashboard into.
+      - name: 'default'
+        orgId: 1
         folder: ''
         type: file
         disableDeletion: false
@@ -50,72 +43,70 @@ grafana:
         gnetId: 3831
         datasource: Prometheus
 prometheus-node-exporter:
-  priorityClassName: ${local.priority_class_ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
+  priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
 prometheus:
   prometheusSpec:
-    priorityClassName: ${local.priority_class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
+    priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
 alertmanager:
   alertmanagerSpec:
-    priorityClassName: ${local.priority_class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
+    priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
 VALUES
 }
 
 
-resource "kubernetes_namespace" "prometheus_operator" {
-  count = local.prometheus_operator["enabled"] ? 1 : 0
+resource "kubernetes_namespace" "kube-prometheus-stack" {
+  count = local.kube-prometheus-stack["enabled"] ? 1 : 0
 
   metadata {
     labels = {
-      name = local.prometheus_operator["namespace"]
-    }
-    annotations = {
-      "iam.amazonaws.com/permitted" = local.kiam["enabled"] ? local.prometheus_operator["kiam_allowed_regexp"] : "^$"
+      name                               = local.kube-prometheus-stack["namespace"]
+      "${local.labels_prefix}/component" = "monitoring"
     }
 
-    name = local.prometheus_operator["namespace"]
+    name = local.kube-prometheus-stack["namespace"]
   }
 }
 
 resource "random_string" "grafana_password" {
-  count   = local.prometheus_operator["enabled"] ? 1 : 0
+  count   = local.kube-prometheus-stack["enabled"] ? 1 : 0
   length  = 16
   special = false
 }
 
-resource "helm_release" "prometheus_operator" {
-  count                 = local.prometheus_operator["enabled"] ? 1 : 0
-  repository            = local.prometheus_operator["repository"]
-  name                  = local.prometheus_operator["name"]
-  chart                 = local.prometheus_operator["chart"]
-  version               = local.prometheus_operator["chart_version"]
-  timeout               = local.prometheus_operator["timeout"]
-  force_update          = local.prometheus_operator["force_update"]
-  recreate_pods         = local.prometheus_operator["recreate_pods"]
-  wait                  = local.prometheus_operator["wait"]
-  atomic                = local.prometheus_operator["atomic"]
-  cleanup_on_fail       = local.prometheus_operator["cleanup_on_fail"]
-  dependency_update     = local.prometheus_operator["dependency_update"]
-  disable_crd_hooks     = local.prometheus_operator["disable_crd_hooks"]
-  disable_webhooks      = local.prometheus_operator["disable_webhooks"]
-  render_subchart_notes = local.prometheus_operator["render_subchart_notes"]
-  replace               = local.prometheus_operator["replace"]
-  reset_values          = local.prometheus_operator["reset_values"]
-  reuse_values          = local.prometheus_operator["reuse_values"]
-  skip_crds             = local.prometheus_operator["skip_crds"]
-  verify                = local.prometheus_operator["verify"]
+resource "helm_release" "kube-prometheus-stack" {
+  count                 = local.kube-prometheus-stack["enabled"] ? 1 : 0
+  repository            = local.kube-prometheus-stack["repository"]
+  name                  = local.kube-prometheus-stack["name"]
+  chart                 = local.kube-prometheus-stack["chart"]
+  version               = local.kube-prometheus-stack["chart_version"]
+  timeout               = local.kube-prometheus-stack["timeout"]
+  force_update          = local.kube-prometheus-stack["force_update"]
+  recreate_pods         = local.kube-prometheus-stack["recreate_pods"]
+  wait                  = local.kube-prometheus-stack["wait"]
+  atomic                = local.kube-prometheus-stack["atomic"]
+  cleanup_on_fail       = local.kube-prometheus-stack["cleanup_on_fail"]
+  dependency_update     = local.kube-prometheus-stack["dependency_update"]
+  disable_crd_hooks     = local.kube-prometheus-stack["disable_crd_hooks"]
+  disable_webhooks      = local.kube-prometheus-stack["disable_webhooks"]
+  render_subchart_notes = local.kube-prometheus-stack["render_subchart_notes"]
+  replace               = local.kube-prometheus-stack["replace"]
+  reset_values          = local.kube-prometheus-stack["reset_values"]
+  reuse_values          = local.kube-prometheus-stack["reuse_values"]
+  skip_crds             = local.kube-prometheus-stack["skip_crds"]
+  verify                = local.kube-prometheus-stack["verify"]
   values = [
-    local.values_prometheus_operator,
-    local.prometheus_operator["extra_values"]
+    local.values_kube-prometheus-stack,
+    local.kube-prometheus-stack["extra_values"]
   ]
-  namespace = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+  namespace = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
 }
 
-resource "kubernetes_network_policy" "prometheus_operator_default_deny" {
-  count = local.prometheus_operator["enabled"] && local.prometheus_operator["default_network_policy"] ? 1 : 0
+resource "kubernetes_network_policy" "kube-prometheus-stack_default_deny" {
+  count = local.kube-prometheus-stack["enabled"] && local.kube-prometheus-stack["default_network_policy"] ? 1 : 0
 
   metadata {
-    name      = "${kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]}-default-deny"
-    namespace = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+    name      = "${kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]}-default-deny"
+    namespace = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
   }
 
   spec {
@@ -125,12 +116,12 @@ resource "kubernetes_network_policy" "prometheus_operator_default_deny" {
   }
 }
 
-resource "kubernetes_network_policy" "prometheus_operator_allow_namespace" {
-  count = local.prometheus_operator["enabled"] && local.prometheus_operator["default_network_policy"] ? 1 : 0
+resource "kubernetes_network_policy" "kube-prometheus-stack_allow_namespace" {
+  count = local.kube-prometheus-stack["enabled"] && local.kube-prometheus-stack["default_network_policy"] ? 1 : 0
 
   metadata {
-    name      = "${kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]}-allow-namespace"
-    namespace = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+    name      = "${kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]}-allow-namespace"
+    namespace = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
   }
 
   spec {
@@ -141,7 +132,7 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_namespace" {
       from {
         namespace_selector {
           match_labels = {
-            name = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+            name = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
           }
         }
       }
@@ -151,12 +142,12 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_namespace" {
   }
 }
 
-resource "kubernetes_network_policy" "prometheus_operator_allow_ingress_nginx" {
-  count = local.prometheus_operator["enabled"] && local.prometheus_operator["default_network_policy"] && local.nginx_ingress["enabled"] ? 1 : 0
+resource "kubernetes_network_policy" "kube-prometheus-stack_allow_ingress" {
+  count = local.kube-prometheus-stack["enabled"] && local.kube-prometheus-stack["default_network_policy"] ? 1 : 0
 
   metadata {
-    name      = "${kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]}-allow-ingress-nginx"
-    namespace = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+    name      = "${kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]}-allow-ingress"
+    namespace = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
   }
 
   spec {
@@ -172,7 +163,7 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_ingress_nginx" {
       from {
         namespace_selector {
           match_labels = {
-            name = kubernetes_namespace.nginx_ingress.*.metadata.0.name[count.index]
+            "${local.labels_prefix}/component" = "ingress"
           }
         }
       }
@@ -182,12 +173,12 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_ingress_nginx" {
   }
 }
 
-resource "kubernetes_network_policy" "prometheus_operator_allow_control_plane" {
-  count = local.prometheus_operator["enabled"] && local.prometheus_operator["default_network_policy"] ? 1 : 0
+resource "kubernetes_network_policy" "kube-prometheus-stack_allow_control_plane" {
+  count = local.kube-prometheus-stack["enabled"] && local.kube-prometheus-stack["default_network_policy"] ? 1 : 0
 
   metadata {
-    name      = "${kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]}-allow-control-plane"
-    namespace = kubernetes_namespace.prometheus_operator.*.metadata.0.name[count.index]
+    name      = "${kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]}-allow-control-plane"
+    namespace = kubernetes_namespace.kube-prometheus-stack.*.metadata.0.name[count.index]
   }
 
   spec {
@@ -195,7 +186,7 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_control_plane" {
       match_expressions {
         key      = "app"
         operator = "In"
-        values   = ["${local.prometheus_operator["name"]}-operator"]
+        values   = ["${local.kube-prometheus-stack["name"]}-operator"]
       }
     }
 
@@ -206,7 +197,7 @@ resource "kubernetes_network_policy" "prometheus_operator_allow_control_plane" {
       }
 
       dynamic "from" {
-        for_each = local.prometheus_operator["allowed_cidrs"]
+        for_each = local.kube-prometheus-stack["allowed_cidrs"]
         content {
           ip_block {
             cidr = from.value
