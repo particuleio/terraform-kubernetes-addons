5f15138a26b9c85d1731fb915d98224198c63d5b
Kevin Lefevre <lefevre.kevin@gmail.com>
2020-12-15 15:16:28 +0100

    fix(aws): do not use proxy protocol with NLB
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- modules/aws/ingress-nginx.tf
+++ modules/aws/ingress-nginx.tf
@@ -60,12 +60,9 @@ controller:
       service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
       service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'
       service.beta.kubernetes.io/aws-load-balancer-type: nlb
-      service.beta.kubernetes.io/aws-load-balancer-proxy-protocol: "*"
     externalTrafficPolicy: "Local"
   publishService:
     enabled: true
-  config:
-    use-proxy-protocol: "true"
   priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
 podSecurityPolicy:
   enabled: true
@@ -87,11 +84,8 @@ controller:
       service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
       service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'
       service.beta.kubernetes.io/aws-load-balancer-type: "nlb-ip"
-      service.beta.kubernetes.io/aws-load-balancer-proxy-protocol: "*"
   publishService:
     enabled: true
-  config:
-    use-proxy-protocol: "true"
   priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
 podSecurityPolicy:
   enabled: true
