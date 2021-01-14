36eb17fe271c0e668879c966352b8da71e9a451a
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-01-14 13:19:22 +0100

    fix: remove hardcoded extra args
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- metrics-server.tf
+++ metrics-server.tf
@@ -20,7 +20,6 @@ image:
   tag: ${local.metrics-server["version"]}
 args:
   - --logtostderr
-  - --kubelet-preferred-address-types=InternalIP,ExternalIP
 rbac:
   pspEnabled: true
 priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
