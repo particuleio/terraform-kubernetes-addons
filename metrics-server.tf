e2c0e00bf97857a08f5126b38bd9dd1b48bae0c4
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-01-14 13:25:45 +0100

    Revert "fix: remove hardcoded extra args"
    This reverts commit 36eb17fe271c0e668879c966352b8da71e9a451a.

--- metrics-server.tf
+++ metrics-server.tf
@@ -20,6 +20,7 @@ image:
   tag: ${local.metrics-server["version"]}
 args:
   - --logtostderr
+  - --kubelet-preferred-address-types=InternalIP,ExternalIP
 rbac:
   pspEnabled: true
 priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
