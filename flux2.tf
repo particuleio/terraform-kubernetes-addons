1701f41848ec40903d2bc66f6b567f0fd0c5fb8b
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-03-04 11:00:06 +0100

    feat: add auto image update logic
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- flux2.tf
+++ flux2.tf
@@ -21,6 +21,8 @@ locals {
       default_components       = ["source-controller", "kustomize-controller", "helm-controller", "notification-controller"]
       components               = []
       provider                 = "github"
+      auto_image_update        = false
+
       known_hosts = [
         "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==",
         "gitlab.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsj2bNKTBSpIYDEGk9KxsGh3mySTRgMtXL583qmBpzeQ+jqCMRgBqB98u3z++J1sKlXHWfM9dyhSevkMwSbhoR8XIq/U0tCNyokEi/ueaBMCvbcTHhO7FcwzY92WK4Yt0aGROY5qX2UKSeOvuP4D6TPqKF1onrSzH9bx9XUf2lEdWT/ia1NEKjunUqu1xOB/StKDHMoX4/OKyIzuS0q/T1zOATthvasJFoPrAjkohTyaDUz2LN5JoH839hViyEG82yB+MjcFV5MU3N1l1QL3cVUCh93xSaua1N85qivl+siMkPGbO5xR/En4iEY6K2XPASUEMaieWVNTRCtJ4S8H+9"
@@ -71,7 +73,7 @@ data "flux_install" "main" {
   target_path    = local.flux2["target_path"]
   network_policy = false
   version        = local.flux2["version"]
-  components     = concat(local.flux2["default_components"], local.flux2["components"])
+  components     = distinct(concat(local.flux2["default_components"], local.flux2["components"], local.flux2["auto_image_update"] ? ["image-reflector-controller", "image-automation-controller"] : []))
 }
 
 # Split multi-doc YAML with
@@ -155,7 +157,7 @@ resource "github_repository_deploy_key" "main" {
   title      = "flux-${local.flux2["create_github_repository"] ? github_repository.main[0].name : local.flux2["repository"]}-${local.flux2["branch"]}"
   repository = local.flux2["create_github_repository"] ? github_repository.main[0].name : data.github_repository.main[0].name
   key        = tls_private_key.identity[0].public_key_openssh
-  read_only  = true
+  read_only  = local.flux2["auto_image_update"]
 }
 
 resource "github_repository_file" "install" {
