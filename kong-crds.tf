9512c60148eb89410d2dcaf77f2faa5abe09b54f
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-04-22 18:49:43 +0200

    fix: only enable CRDs when component is enable
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- kong-crds.tf
+++ kong-crds.tf
@@ -4,7 +4,7 @@ locals {
 
   kong_crds = "https://raw.githubusercontent.com/Kong/charts/${local.kong_crd_version}/charts/kong/crds/custom-resource-definitions.yaml"
 
-  kong_crds_apply = local.kong.manage_crds ? [for v in data.kubectl_file_documents.kong_crds.0.documents : {
+  kong_crds_apply = local.kong.enabled && local.kong.manage_crds ? [for v in data.kubectl_file_documents.kong_crds.0.documents : {
     data : yamldecode(v)
     content : v
     }
@@ -12,16 +12,16 @@ locals {
 }
 
 data "http" "kong_crds" {
-  count = local.kong.manage_crds ? 1 : 0
+  count = local.kong.enabled && local.kong.manage_crds ? 1 : 0
   url   = local.kong_crds
 }
 
 data "kubectl_file_documents" "kong_crds" {
-  count   = local.kong.manage_crds ? 1 : 0
+  count   = local.kong.enabled && local.kong.manage_crds ? 1 : 0
   content = data.http.kong_crds[0].body
 }
 
 resource "kubectl_manifest" "kong_crds" {
-  for_each  = local.kong.manage_crds ? { for v in local.kong_crds_apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content } : {}
+  for_each  = local.kong.enabled && local.kong.manage_crds ? { for v in local.kong_crds_apply : lower(join("/", compact([v.data.apiVersion, v.data.kind, lookup(v.data.metadata, "namespace", ""), v.data.metadata.name]))) => v.content } : {}
   yaml_body = each.value
 }
--- kube-prometheus-crd.tf
+++ kube-prometheus-crd.tf
@@ -1,6 +1,6 @@
 locals {
 
-  prometheus-operator_crd_version = local.kube-prometheus-stack.manage_crds ? yamldecode(data.http.prometheus-operator_version.0.body).appVersion : ""
+  prometheus-operator_crd_version = local.kube-prometheus-stack.enabled && local.kube-prometheus-stack.manage_crds ? yamldecode(data.http.prometheus-operator_version.0.body).appVersion : ""
 
   prometheus-operator_crds = [
     "https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v${local.prometheus-operator_crd_version}/example/prometheus-operator-crd/monitoring.coreos.com_alertmanagerconfigs.yaml",
@@ -18,16 +18,16 @@ locals {
 }
 
 data "http" "prometheus-operator_version" {
-  count = local.kube-prometheus-stack.manage_crds ? 1 : 0
+  count = local.kube-prometheus-stack.enabled && local.kube-prometheus-stack.manage_crds ? 1 : 0
   url   = local.prometheus-operator_chart
 }
 
 data "http" "prometheus-operator_crds" {
-  for_each = local.kube-prometheus-stack.manage_crds ? toset(local.prometheus-operator_crds) : []
+  for_each = local.kube-prometheus-stack.enabled && local.kube-prometheus-stack.manage_crds ? toset(local.prometheus-operator_crds) : []
   url      = each.key
 }
 
 resource "kubectl_manifest" "prometheus-operator_crds" {
-  for_each  = data.http.prometheus-operator_crds
+  for_each  = local.kube-prometheus-stack.enabled && local.kube-prometheus-stack.manage_crds ? data.http.prometheus-operator_crds : []
   yaml_body = each.value.body
 }
