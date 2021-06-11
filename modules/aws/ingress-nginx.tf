65dd620a1a1425cfbc715f149b749592d93fdd0a
Kevin Lefevre <kevin@particule.io>
2021-06-11 17:59:04 +0200

    Update ingress-nginx.tf

--- modules/aws/ingress-nginx.tf
+++ modules/aws/ingress-nginx.tf
@@ -55,6 +55,7 @@ controller:
       service.beta.kubernetes.io/aws-load-balancer-backend-protocol: tcp
       service.beta.kubernetes.io/aws-load-balancer-cross-zone-load-balancing-enabled: 'true'
       service.beta.kubernetes.io/aws-load-balancer-type: nlb
+      service.beta.kubernetes.io/aws-load-balancer-scheme: internet-facing
     externalTrafficPolicy: "Local"
   publishService:
     enabled: true
