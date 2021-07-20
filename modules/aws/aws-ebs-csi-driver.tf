b3b4345fc919c0f190e54d8e8120686b3cb28193
Kevin Lefevre <lefevre.kevin@gmail.com>
2021-07-20 17:29:11 +0200

    fix: deprecated chart options for ebs-csi-driver
    Signed-off-by: Kevin Lefevre <lefevre.kevin@gmail.com>

--- modules/aws/aws-ebs-csi-driver.tf
+++ modules/aws/aws-ebs-csi-driver.tf
@@ -10,7 +10,7 @@ locals {
       create_ns     = false
       service_account_names = {
         controller = "ebs-csi-controller-sa"
-        snapshot   = "ebs-snapshot-controller"
+        node       = "ebs-csi-node-sa"
       }
       create_iam_resources_irsa = true
       create_storage_class      = true
@@ -28,19 +28,18 @@ enableVolumeScheduling: true
 enableVolumeResizing: true
 enableVolumeSnapshot: true
 extraCreateMetadata: true
-priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
-node:
-  tolerateAllTaints: true
-  priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
 k8sTagClusterId: ${var.cluster-name}
-serviceAccount:
-  controller:
+controller:
+  priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
+  serviceAccount:
     name: ${local.aws-ebs-csi-driver["service_account_names"]["controller"]}
     annotations:
       eks.amazonaws.com/role-arn: "${local.aws-ebs-csi-driver["enabled"] && local.aws-ebs-csi-driver["create_iam_resources_irsa"] ? module.iam_assumable_role_aws-ebs-csi-driver.iam_role_arn : ""}"
-  snapshot:
-    create: true
-    name: ${local.aws-ebs-csi-driver["service_account_names"]["snapshot"]}
+node:
+  tolerateAllTaints: true
+  priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
+  serviceAccount:
+    name: ${local.aws-ebs-csi-driver["service_account_names"]["node"]}
     annotations:
       eks.amazonaws.com/role-arn: "${local.aws-ebs-csi-driver["enabled"] && local.aws-ebs-csi-driver["create_iam_resources_irsa"] ? module.iam_assumable_role_aws-ebs-csi-driver.iam_role_arn : ""}"
 VALUES
