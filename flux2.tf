a204033d47925f1534b1685b8ba1819c3b96b74c
Kevin Lefevre <kevin@particule.io>
2021-11-08 18:06:35 +0100

    feat: update flux to v0.21
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- flux2.tf
+++ flux2.tf
@@ -11,7 +11,7 @@ locals {
       namespace                = "flux-system"
       target_path              = "production"
       default_network_policy   = true
-      version                  = "v0.19.0"
+      version                  = "v0.21.1"
       github_url               = "ssh://git@<host>/<org>/<repository>"
       create_github_repository = false
       github_token             = ""
