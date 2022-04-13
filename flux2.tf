24dd4adcc6bb6704498ad70239432c8a8ae24cfa
Kevin Lefevre <kevin@particule.io>
2022-04-13 13:26:30 +0200

    feat: update default flux version to v0.28.5
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- flux2.tf
+++ flux2.tf
@@ -11,7 +11,7 @@ locals {
       namespace                = "flux-system"
       target_path              = "production"
       default_network_policy   = true
-      version                  = "v0.28.2"
+      version                  = "v0.28.5"
       github_url               = "ssh://git@<host>/<org>/<repository>"
       create_github_repository = false
       github_token             = ""
