f44b94b832d3800fa4dac44c7bd3eaea75154097
Kevin Lefevre <kevin@particule.io>
2022-10-10 14:30:33 +0200

    fix(loki): disable loki canary
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- modules/aws/loki-stack.tf
+++ modules/aws/loki-stack.tf
@@ -31,6 +31,8 @@ locals {
         enabled: false
         grafanaAgent:
           installOperator: false
+        lokiCanary:
+          enabled: false
     serviceMonitor:
       enabled: ${local.kube-prometheus-stack["enabled"] || local.victoria-metrics-k8s-stack["enabled"]}
     priorityClassName: ${local.priority-class["create"] ? kubernetes_priority_class.kubernetes_addons[0].metadata[0].name : ""}
