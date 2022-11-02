ef8134df01fb94704cbdb35be2149402edd47cfa
Kevin Lefevre <kevin@particule.io>
2022-11-02 13:47:55 +0100

    feat: update flux provider to v0.20
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- flux2.tf
+++ flux2.tf
@@ -11,7 +11,7 @@ locals {
       namespace                = "flux-system"
       target_path              = "production"
       default_network_policy   = true
-      version                  = "v0.35.0"
+      version                  = "v0.36.0"
       github_url               = "ssh://git@<host>/<org>/<repository>"
       create_github_repository = false
       github_token             = ""
