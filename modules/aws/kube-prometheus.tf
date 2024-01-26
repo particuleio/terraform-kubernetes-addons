529cf13f5ea4974621e9ba3af18cecf92290ad7a
Kevin Lefevre <kevin@particule.io>
2024-01-26 18:07:12 +0100

    feat: update thanos version
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- modules/aws/kube-prometheus.tf
+++ modules/aws/kube-prometheus.tf
@@ -19,7 +19,7 @@ locals {
       thanos_bucket                     = "thanos-store-${var.cluster-name}"
       thanos_bucket_force_destroy       = false
       thanos_store_config               = null
-      thanos_version                    = "v0.32.5"
+      thanos_version                    = "v0.33.0"
       enabled                           = false
       allowed_cidrs                     = ["0.0.0.0/0"]
       default_network_policy            = true
