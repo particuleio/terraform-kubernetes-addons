7e501f3696c27c0224941f4f6c4634287d9de51b
Kevin Lefevre <kevin@particule.io>
2024-01-29 10:53:42 +0000

    feat: add support for custom labels and annotations on ingress-nginx (#2563)
    Signed-off-by: Kevin Lefevre <kevin@particule.io>

--- modules/google/ingress-nginx.tf
+++ modules/google/ingress-nginx.tf
@@ -13,6 +13,8 @@ locals {
       default_network_policy = true
       ingress_cidrs          = ["0.0.0.0/0"]
       allowed_cidrs          = ["0.0.0.0/0"]
+      extra_ns_labels        = {}
+      extra_ns_annotations   = {}
     },
     var.ingress-nginx
   )
@@ -71,10 +73,15 @@ resource "kubernetes_namespace" "ingress-nginx" {
   count = local.ingress-nginx["enabled"] ? 1 : 0
 
   metadata {
-    labels = {
+    labels = merge({
       name                               = local.ingress-nginx["namespace"]
       "${local.labels_prefix}/component" = "ingress"
-    }
+      },
+    local.ingress-nginx["extra_ns_labels"])
+
+    annotations = merge(
+      local.ingress-nginx["extra_ns_annotations"]
+    )
 
     name = local.ingress-nginx["namespace"]
   }
