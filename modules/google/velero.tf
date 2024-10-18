2d82d602c4200756aa57de087a96176349dab4f2
Kevin Lefevre <archi@kiln.fi>
2024-10-18 13:58:22 +0200

    fix(google): velero condition
    Signed-off-by: Kevin Lefevre <archi@kiln.fi>

--- modules/google/velero.tf
+++ modules/google/velero.tf
@@ -49,7 +49,7 @@ serviceAccount:
     name: ${local.velero["service_account_name"]}
     create: true
     annotations:
-      iam.gke.io/gcp-service-account: ${local.velero["create_iam_account"] ? google_service_account.velero[0].email : ""}
+       ${local.velero["create_iam_account"] ? "iam.gke.io/gcp-service-account: ${google_service_account.velero[0].email}" : ""}
 priorityClassName: ${local.priority-class-ds["create"] ? kubernetes_priority_class.kubernetes_addons_ds[0].metadata[0].name : ""}
 credentials:
   useSecret: false
