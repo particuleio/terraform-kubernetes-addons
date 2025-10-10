d1102e212333ed257218caa386aec4ff8e1e6f44
Mehdi Bechiri <cebidhem@pm.me>
2025-10-10 21:51:10 +0200

    feat!(flux): migrate to flux operator and flux instance
    Signed-off-by: Mehdi Bechiri <cebidhem@pm.me>

--- flux2.tf
+++ flux2.tf
@@ -1,26 +1,73 @@
 locals {
 
-  # GITHUB_TOKEN should be set for Github provider to work
-  # GITHUB_ORGANIZATION should be set if deploying in another ORG and not your
-  # github user
+  flux-operator = merge(
+    local.helm_defaults,
+    {
+      name                   = "flux-operator"
+      chart                  = local.helm_dependencies[index(local.helm_dependencies.*.name, "oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator")].name
+      repository             = ""
+      chart_version          = local.helm_dependencies[index(local.helm_dependencies.*.name, "oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator")].version
+      enabled                = false
+      create_ns              = true
+      namespace              = "flux-system"
+      extra_ns_labels        = {}
+      extra_ns_annotations   = {}
+      default_network_policy = true
+    },
+    var.flux-operator
+  )
+
+  values_flux-operator = <<VALUES
+    serviceMonitor:
+      enabled: ${local.kube-prometheus-stack["enabled"] || local.victoria-metrics-k8s-stack["enabled"]}
+  VALUES
 
   flux2 = merge(
+    local.helm_defaults,
     {
-      enabled                  = false
-      create_ns                = true
-      namespace                = "flux-system"
-      path                     = "gitops/clusters/${var.cluster-name}"
-      version                  = "v2.6.1"
-      create_github_repository = false
-      repository               = "gitops"
-      repository_visibility    = "public"
-      branch                   = "main"
-      components_extra         = ["image-reflector-controller", "image-automation-controller"]
-      read_only                = false
-      default_network_policy   = true
+      name                       = "flux"
+      chart                      = local.helm_dependencies[index(local.helm_dependencies.*.name, "oci://ghcr.io/controlplaneio-fluxcd/charts/flux-instance")].name
+      repository                 = ""
+      chart_version              = local.helm_dependencies[index(local.helm_dependencies.*.name, "oci://ghcr.io/controlplaneio-fluxcd/charts/flux-instance")].version
+      enabled                    = false
+      create_ns                  = true
+      namespace                  = "flux-system"
+      version                    = "v2.6.1"
+      cluster_type               = "kubernetes"
+      cluster_size               = "medium"
+      git_path                   = "gitops/clusters/${var.cluster-name}"
+      git_ref                    = "refs/heads/main"
+      git_token                  = ""
+      github_app_id              = ""
+      github_app_installation_id = ""
+      github_app_pem             = ""
+      components                 = ["source-controller", "kustomize-controller", "helm-controller", "notification-controller", "image-reflector-controller", "image-automation-controller"]
+      create_github_repository   = false
+      repository                 = "gitops"
+      repository_visibility      = "public"
+      default_network_policy     = true
     },
     var.flux2
   )
+
+  values_flux2 = <<VALUES
+    instance:
+      components: ${local.flux2["components"]}
+      distribution:
+        version: ${local.flux2["flux_version"]}
+      cluster:
+        type: ${local.flux2["cluster_type"]}
+        size: ${local.flux2["cluster_size"]}
+      sync:
+        kind: GitRepository
+        url: ${local.flux2["create_github_repository"]} ? ${github_repository.main[0].name} : ${data.github_repository.main[0].name}
+        path: ${local.flux2["git_path"]}
+        ref: ${local.flux2["git_ref"]}
+        provider: ${local.flux2["github_app_id"] != "" ? "github" : "generic"}
+        pullSecret: ${local.flux2["git_token"] != "" || local.flux2["github_app_id"] != "" ? "flux-system" : ""}
+    healthcheck:
+      enabled: true
+ VALUES
 }
 
 resource "kubernetes_namespace" "flux2" {
@@ -41,10 +88,88 @@ resource "kubernetes_namespace" "flux2" {
   }
 }
 
-resource "tls_private_key" "identity" {
-  count       = local.flux2["enabled"] ? 1 : 0
-  algorithm   = "ECDSA"
-  ecdsa_curve = "P521"
+// Create a Kubernetes secret with the Git credentials
+// if a GitHub/GitLab token or GitHub App is provided.
+
+resource "kubernetes_secret" "git_auth" {
+  count      = local.flux2["git_token"] != "" || local.flux2["github_app_id"] != "" ? 1 : 0
+  depends_on = [kubernetes_namespace.flux2]
+
+  metadata {
+    name      = "flux-system"
+    namespace = local.flux2["namespace"]
+  }
+
+  data = {
+    username                = local.flux2["git_token"] != "" ? "git" : null
+    password                = local.flux2["git_token"] != "" ? local.flux2["git_token"] : null
+    githubAppID             = local.flux2["github_app_id"] != "" ? local.flux2["github_app_id"] : null
+    githubAppInstallationID = local.flux2["github_app_installation_id"] != "" ? local.flux2["github_app_installation_id"] : null
+    githubAppPrivateKey     = local.flux2["github_app_pem"] != "" ? local.flux2["github_app_pem"] : null
+  }
+
+  type = "Opaque"
+}
+
+// Install the Flux Operator.
+resource "helm_release" "flux_operator" {
+  depends_on = [kubernetes_namespace.flux2]
+
+  count                 = local.flux-operator["enabled"] ? 1 : 0
+  repository            = local.flux-operator["repository"]
+  name                  = local.flux-operator["name"]
+  chart                 = local.flux-operator["chart"]
+  version               = local.flux-operator["chart_version"]
+  timeout               = local.flux-operator["timeout"]
+  force_update          = local.flux-operator["force_update"]
+  recreate_pods         = local.flux-operator["recreate_pods"]
+  wait                  = local.flux-operator["wait"]
+  atomic                = local.flux-operator["atomic"]
+  cleanup_on_fail       = local.flux-operator["cleanup_on_fail"]
+  dependency_update     = local.flux-operator["dependency_update"]
+  disable_crd_hooks     = local.flux-operator["disable_crd_hooks"]
+  disable_webhooks      = local.flux-operator["disable_webhooks"]
+  render_subchart_notes = local.flux-operator["render_subchart_notes"]
+  replace               = local.flux-operator["replace"]
+  reset_values          = local.flux-operator["reset_values"]
+  reuse_values          = local.flux-operator["reuse_values"]
+  skip_crds             = local.flux-operator["skip_crds"]
+  verify                = local.flux-operator["verify"]
+  values = [
+    local.values_flux-operator,
+    local.flux-operator["extra_values"],
+  ]
+  namespace = kubernetes_namespace.flux2.*.metadata.0.name[count.index]
+}
+
+resource "helm_release" "flux2" {
+  depends_on = [helm_release.flux_operator]
+
+  count                 = local.flux2["enabled"] ? 1 : 0
+  repository            = local.flux2["repository"]
+  name                  = local.flux2["name"]
+  chart                 = local.flux2["chart"]
+  version               = local.flux2["chart_version"]
+  timeout               = local.flux2["timeout"]
+  force_update          = local.flux2["force_update"]
+  recreate_pods         = local.flux2["recreate_pods"]
+  wait                  = local.flux2["wait"]
+  atomic                = local.flux2["atomic"]
+  cleanup_on_fail       = local.flux2["cleanup_on_fail"]
+  dependency_update     = local.flux2["dependency_update"]
+  disable_crd_hooks     = local.flux2["disable_crd_hooks"]
+  disable_webhooks      = local.flux2["disable_webhooks"]
+  render_subchart_notes = local.flux2["render_subchart_notes"]
+  replace               = local.flux2["replace"]
+  reset_values          = local.flux2["reset_values"]
+  reuse_values          = local.flux2["reuse_values"]
+  skip_crds             = local.flux2["skip_crds"]
+  verify                = local.flux2["verify"]
+  values = [
+    local.values_flux2,
+    local.flux2["extra_values"],
+  ]
+  namespace = kubernetes_namespace.flux2.*.metadata.0.name[count.index]
 }
 
 data "github_repository" "main" {
@@ -63,42 +188,6 @@ resource "github_branch_default" "main" {
   count      = local.flux2["enabled"] && local.flux2["create_github_repository"] ? 1 : 0
   repository = local.flux2["create_github_repository"] ? github_repository.main[0].name : data.github_repository.main[0].name
   branch     = local.flux2["branch"]
-}
-
-resource "github_repository_deploy_key" "main" {
-  count      = local.flux2["enabled"] ? 1 : 0
-  title      = "flux-${local.flux2["create_github_repository"] ? github_repository.main[0].name : local.flux2["repository"]}-${local.flux2["branch"]}"
-  repository = local.flux2["create_github_repository"] ? github_repository.main[0].name : data.github_repository.main[0].name
-  key        = tls_private_key.identity[0].public_key_openssh
-  read_only  = local.flux2["read_only"]
-}
-
-resource "flux_bootstrap_git" "flux" {
-  count = local.flux2["enabled"] ? 1 : 0
-
-  depends_on = [
-    github_repository_deploy_key.main,
-    kubernetes_namespace.flux2
-  ]
-
-  path                    = local.flux2["path"]
-  version                 = local.flux2["version"]
-  namespace               = local.flux2["namespace"]
-  cluster_domain          = try(local.flux2["cluster_domain"], null)
-  components              = try(local.flux2["components"], null)
-  components_extra        = try(local.flux2["components_extra"], null)
-  disable_secret_creation = try(local.flux2["disable_secret_creation"], null)
-  image_pull_secret       = try(local.flux2["image_pull_secrets"], null)
-  interval                = try(local.flux2["interval"], null)
-  kustomization_override  = try(local.flux2["kustomization_override"], null)
-  log_level               = try(local.flux2["log_level"], null)
-  network_policy          = try(local.flux2["network_policy"], null)
-  recurse_submodules      = try(local.flux2["recurse_submodules"], null)
-  registry                = try(local.flux2["registry"], null)
-  secret_name             = try(local.flux2["secret_name"], null)
-  toleration_keys         = try(local.flux2["toleration_keys"], null)
-  watch_all_namespaces    = try(local.flux2["watch_all_namespaces"], null)
-
 }
 
 resource "kubernetes_network_policy" "flux2_allow_monitoring" {
